22d58ad7c9efbfae9a525c4e2e9333e759cacc2c
Sujith <Sujith@PERI>
2025-07-26 18:06:23 +0100

    Add project files.

---
+++ Helper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace AlgorithmsTest
+{
+    public class Helper
+    {
+
+        //global Fields & properties
+
+        private string algorithmname;
+        public string AlgorithmName
+        {
+            get => algorithmname;
+            set => algorithmname = value;
+        }
+
+        private double algorithmvalue1;
+        public double AlgorithmValue1
+        {
+            get => algorithmvalue1;
+            set => algorithmvalue1 = value;
+        }
+
+        private double algorithmvalue2;
+        public double AlgorithmValue2
+        {
+            get => algorithmvalue2;
+            set => algorithmvalue2 = value;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Reflection;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography;
+
+namespace AlgorithmsTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Clear();
+            Console.WriteLine("Please enter the Algorithm Name,Value 1 , Value 2 \n For Example : For BMI please Enter BMI,  Height in Centimeters and Weight in Kilos in the format below \n BMI, 180, 78.6");
+            string[] SplitAlgorithmArguments = Console.ReadLine().Split(','); // I have googled the split syntax , I remember the keywords IndexOf and Split
+            Helper help = new Helper();
+            try // want to catch the exception in case the input was in a different format or any other error
+            {
+                help.AlgorithmName = (SplitAlgorithmArguments[0]);
+                help.AlgorithmValue1 = Convert.ToDouble((SplitAlgorithmArguments[1]));
+                help.AlgorithmValue2 = Convert.ToDouble((SplitAlgorithmArguments[2]));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + "Please try again or contact support team");
+                System.Environment.Exit(0);
+            }
+
+
+            // Googled , if i can Dynamically input algorithm name with some appended string to form the method name , check for the method if exists and execute 
+            // helpful if there are hundreds of methods to check for instead of using if / switch conditions in the algorithms
+
+            Algorithms algorithms = new Algorithms();
+
+            // approach 1 : googled this approach
+
+
+            try
+            {
+                var getmethod = algorithms.GetType();
+                bool DoesTheMethodExists = getmethod.GetMethod(help.AlgorithmName + "Algorithm") != null;
+                if (DoesTheMethodExists == true)
+                {
+                    MethodInfo method = typeof(Algorithms).GetMethod(help.AlgorithmName + "Algorithm");
+                    object result = method?.Invoke(algorithms, new object[] { help.AlgorithmValue1, help.AlgorithmValue2 });
+                    Console.WriteLine(result);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+
+            // appraoch 2
+            // 
+            try
+            {
+
+                if (help.AlgorithmName == "BMI")
+                {
+                    Console.WriteLine(algorithms.BMIAlgorithm(help.AlgorithmValue1, help.AlgorithmValue2));
+                }
+                else
+                {
+                    Console.WriteLine("Try again");
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            // approach 3 can explore Switch / Case 
+
+        }
+    }
+}
